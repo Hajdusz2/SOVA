2a26232480fcc5f6b681fa4fc3a30c09d384313b
Bence Szilveszter Hajdu <abence2@gmail.com>
2019-10-08 12:31:32 +0200

    Initial setup
    DBConnection created and QA and Framework Controllers generated.

---
+++ SOVA/SOVA/DBConnect.cs
@@ -0,0 +1,13 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOVA
+{
+    public static class DBConnect
+    {
+        static readonly NpgsqlConnection connection = new NpgsqlConnection("Server=localhost; Port=5432; User Id=SOVAAPI; Password=rawdata; Database=stackoverflow");
+    }
+}
